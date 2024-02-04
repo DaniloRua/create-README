e2a2b43a51fea9ca8ee3572a2fe84d961bb476c1
Danilopc <sr_danilo@hotmail.com>
2024-02-04 11:25:57 +0000

    questios + generateMarkdown.js added

--- index.js
+++ index.js
@@ -1,21 +1,60 @@
 const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
+const util = require('util');
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// array of questions for user
-const questions = [
+const writeToFile = util.promisify(fs.writeFile)
 
-];
+
+// array of questions for user
+const questions = () => {
+    inquirer.prompt([
+{
+    type: 'input',
+    name: 'title',
+    message: 'What is the name of the repo?'
+},
+{
+  type: 'input',
+  name: 'user',
+  message: 'What is the name of your GitHub?'
+},
+{
+  type: 'input',
+  name: 'email',
+  message: 'What is your email address?'
+},
+{
+    type: 'input',
+    name: 'overview',
+    message: 'What is this project is about?'
+},
+{
+    type: 'input',
+    name: 'license',
+    message: 'What license is using?',
+    default: 'MIT'
+}
+
+]
+)}
 
 // function to write README file
 function writeToFile(fileName, data) {
+
 }
 
 // function to initialize program
 function init() {
-    
-}
+  
+   
+  }
 
 // function call to initialize program
+
 init();
+
+
+//promisify*
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,7 +2,17 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  ## Overview
+${data.overview}
+
+## License
+Licensed under the [${data.license}](https://github.com/${data.user}/${data.title}/blob/master/LICENSE.txt) license.
+
+## Contact
+- GitHub: [${data.user}](https://github.com/${data.user})
+- Email: <${data.email}>
+`
+
 }
 
 module.exports = generateMarkdown;
